59a71ae89cea09d59d436a8fc9beb3cad10e4a55
NitinBokade <nitinbokade46@gmail.com>
2024-11-30 11:30:51 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,8 @@
+var name="Nitin Keshav Bokade ";
+let age =19
+console.log(name,age);
+var standard="third semester";
+var fname ="Nitin"; 
+var Mname ="keshav";
+var lname ="bokade";
+let mobno=9380852512;
